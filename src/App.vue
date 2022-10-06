3d751cf581f93962573ee46ffabd2031079ab8eb
victor-crac <82319700+victor-crac@users.noreply.github.com>
2022-10-06 09:55:40 +0300

    VueJS3 first

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
+/* eslint-disable */
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js"/>
 </template>
 
 <script>
